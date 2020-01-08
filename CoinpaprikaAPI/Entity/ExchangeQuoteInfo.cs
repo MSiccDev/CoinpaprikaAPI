1c139bcae990abd1328639f9236cc79a5718cec6
MSicc <msiccdev@hotmail.com>
2020-01-08 08:07:42 +0100

    updated entity models

--- CoinpaprikaAPI/Entity/ExchangeQuoteInfo.cs
+++ CoinpaprikaAPI/Entity/ExchangeQuoteInfo.cs
@@ -7,10 +7,26 @@ namespace CoinpaprikaAPI.Entity
 {
     public class ExchangeQuoteInfo
     {
-        [JsonProperty("reported_volume_24h")]
-        public long ReportedVolume24H { get; set; }
+        #region Public Properties
 
         [JsonProperty("adjusted_volume_24h")]
         public long AdjustedVolume24H { get; set; }
+
+        [JsonProperty("adjusted_volume_30d")]
+        public long AdjustedVolume30D { get; set; }
+
+        [JsonProperty("adjusted_volume_7d")]
+        public long AdjustedVolume7D { get; set; }
+
+        [JsonProperty("reported_volume_24h")]
+        public long ReportedVolume24H { get; set; }
+
+        [JsonProperty("reported_volume_30d")]
+        public long ReportedVolume30D { get; set; }
+
+        [JsonProperty("reported_volume_7d")]
+        public long ReportedVolume7D { get; set; }
+
+        #endregion Public Properties
     }
 }
